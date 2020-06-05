fe3adc1462ad624c66d547f7d5efbb1591c2cf18
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-06-05 19:59:39 -0300

    exercício corrigido

--- desafio008.py
+++ desafio008.py
@@ -1,5 +1,6 @@
-num = float(input('Digite um valor: '))
-cent = num * 100
-mil = num * 1000
-print(f'O valor {num} em centímetros é: ', cent)
-print(f'O valor de {num} em milímetros é: ', mil)
+num = float(input('Digite um valor em metros: '))
+cm = num * 100
+mm = num * 1000
+print(f'O valor {num} em centímetros é: ', cm)
+print(f'O valor de {num} em milímetros é: ', mm)
+
