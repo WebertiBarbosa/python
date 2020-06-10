a03f2afab24d95df0f48f23906ced278bc492f31
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-06-09 23:24:28 -0300

    Create aula009.py

---
+++ aula009.py
@@ -0,0 +1,27 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+print(frase.count('o'))
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase.replace('Python', 'Android'))
+print(frase)
+#frase = frase.replace('Python', 'Android')
+#print(frase)
+print('Curso' in frase)
+print(frase.find('Vídeo'))
+print(frase.find('vídeo'))
+dividido = frase.split()
+print(dividido)
+print(dividido[0])
+print(dividido[2][3])
+print('-'.join(dividido))
+print('- '.join(dividido))
+print(''.join(dividido))
+print(' '.join(dividido))
+
+
+
+
+
+
