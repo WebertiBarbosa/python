640a70c327c262d4e867a4b4620ca50d42398c00
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-06-13 20:53:15 -0300

    Create desafio022.py

---
+++ desafio022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print(f'Seu nome em minúsculas é {nome.lower()}')
+print(f"Seu nome tem ao todo {len(nome)-nome.count(' ')} letras")
+lista = nome.split()
+print(f'Seu primeiro nome é {lista[0]} e ele tem {len(lista[0])} letras')
